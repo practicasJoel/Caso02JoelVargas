201f9bcb1d2fb6be564e37fcda9dee8959b22cfb
JoelVR17 <vermudo.com@gmail.com>
2022-11-09 18:31:35 -0600

    Primer commit

---
+++ G5_CASO2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace G5_CASO2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ G5_CASO2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="G5_CASO2.MvcApplication" Language="C#" %>
